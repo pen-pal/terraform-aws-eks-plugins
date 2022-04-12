9ad44e584d18babfe9e690b6a8940ed8c06e98e3
pen-pal <unameme@protonmail.com>
2022-04-12 22:39:40 +0545

    improvement and fixes

---
+++ data_source.tf
@@ -0,0 +1,15 @@
+data "aws_region" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_id
+}
+
+data "aws_elb_service_account" "main" {}
