40a7e26ca8a9bf400eaa25f35d72584735f73c4d
pen-pal <61139563+pen-pal@users.noreply.github.com>
2022-05-18 18:00:00 +0545

    udpate provider.tf to exec with aws cli for token

--- provider.tf
+++ provider.tf
@@ -2,7 +2,11 @@
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_id]
+  }
 }
 
 ### configure helm provider
@@ -10,6 +14,10 @@ provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
-  }
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_id]
+    }
+  }  
 }
